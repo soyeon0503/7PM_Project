02e5908ce4645750c4b7526095be840817077d8b
ChaeEun-K <57622711+ChaeEun-K@users.noreply.github.com>
2020-06-13 06:57:22 +0900

    assets
    추가추가

---
+++ New Unity Project/Assets/Sealcub/Scripts/Sealcub.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sealcub : MonoBehaviour {
+    Animator sealcub;
+    private IEnumerator coroutine;
+	// Use this for initialization
+	void Start () {
+        sealcub = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.S))
+        {
+            sealcub.SetBool("idle", true);
+            sealcub.SetBool("walk", false);
+            sealcub.SetBool("walkleft", false);
+            sealcub.SetBool("walkright", false);
+            sealcub.SetBool("upside", false);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            sealcub.SetBool("walk", true);
+            sealcub.SetBool("idle", false);
+            sealcub.SetBool("walkleft", false);
+            sealcub.SetBool("walkright", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            sealcub.SetBool("walkleft", true);
+            sealcub.SetBool("walkright", false);
+            sealcub.SetBool("walk", false);
+            sealcub.SetBool("idle", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            sealcub.SetBool("walkleft", false);
+            sealcub.SetBool("walkright", true);
+            sealcub.SetBool("walk", false);
+            sealcub.SetBool("idle", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            sealcub.SetBool("upside", true);
+            sealcub.SetBool("idle", false);
+            sealcub.SetBool("backward", false);
+            sealcub.SetBool("walk", false);
+            sealcub.SetBool("walkleft", false);
+            sealcub.SetBool("walkright", false);
+            StartCoroutine("scratch");
+            scratch();
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            sealcub.SetBool("backward", true);
+            sealcub.SetBool("scratch", false);
+            sealcub.SetBool("upside", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            sealcub.SetBool("call", true);
+            sealcub.SetBool("idle", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            sealcub.SetBool("die", true);
+            sealcub.SetBool("idle", false);
+        }
+    }
+    IEnumerator idle()
+    {
+        yield return new WaitForSeconds(1.0f);
+        sealcub.SetBool("backward", false);
+        sealcub.SetBool("call", false);
+        sealcub.SetBool("idle", true);
+    }
+    IEnumerator scratch()
+    {
+        yield return new WaitForSeconds(1.0f);
+        sealcub.SetBool("upside", false);
+        sealcub.SetBool("scratch", true);
+        sealcub.SetBool("backward", false);
+    }
+}
---
+++ New Unity Project/Assets/Sealcub/Sealcub_readme.txt
@@ -0,0 +1,44 @@
+Sealcub 3D model.
+
+Include scene demo
+9 Animations in root motion
+2 Textures (4096x4096)
+1 Materials
+1 Prefab
+1 Skybox
+2 Scripts
+
+Low poly model
+
+2780 quad polygons on this mesh
+2782 vertices on this mesh
+
+Textures
+Sealcub_D.png
+Sealcub_N.png
+
+Animations List
+backward
+call
+die
+idle
+scratch
+upside
+walk
+walkleft
+walkright
+
+Scene Demo
+keys for enable animations
+
+1=upside
+2=scratch
+3=backward
+0=die
+-----
+W=walk
+A=walkleft
+D=walkright
+S=idle
+-----
+enjoy game age:)
