740b9594f6117e0e2d7dcade2eb54252d5633107
youngyoungseo <63586399+youngyoungseo@users.noreply.github.com>
2020-06-16 10:23:54 +0900

    dd
    d

---
+++ RotationCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class RotationCtrl : MonoBehaviour
+{
+    Transform parentObj;
+    Vector3 startPos;
+    Vector3 originalPos;
+
+    void Start()
+    {
+        startPos = transform.localPosition;
+        originalPos = transform.localPosition;
+        parentObj = transform.root;
+ 
+    }
+
+    void Update()
+    {
+        ResetVR();
+    }
+
+    void ResetVR()
+    {
+        if (parentObj != null)
+        {
+            startPos -= InputTracking.GetLocalPosition(XRNode.CenterEye);
+
+            Quaternion tempRot = Quaternion.Inverse(parentObj.localRotation);
+            Vector3 newAngle = tempRot.eulerAngles;
+            transform.localEulerAngles = new Vector3(55,originalPos.y, originalPos.z);
+        }
+    }
+
+}
